2afb5213ef690d8808a5d56b258d5c8417bbc28e
liammcluckie <liammcluckie@hotmail.co.uk>
2021-09-28 16:50:32 +0000

    Deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn boutique_ado_project.wsgi:application
--- boutique_ado_project/settings.py
+++ boutique_ado_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0oh)_gd@av6=9sidt)ravgbukq!edp3h0wnmx(&tig8vpd%p9x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['liam-boutique-ado-ci-project', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'boutique_ado_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
